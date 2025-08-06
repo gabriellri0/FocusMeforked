b87e8d4f760e5ab3c4b7f93e0a0fb0f650f4d281
Gabriel Rio Tinto Cavalcanti <griotintocavalcanti@gmail.com>
2025-08-06 20:13:05 -0300

    backend link novo

--- frontend/src/componentes/api.js
+++ frontend/src/componentes/api.js
@@ -1,5 +1,1 @@
-const isLocalhost = window. location. hostname == "localhost";
-
-export const baseURL = isLocalhost
-? "http://localhost:8000/" // Localhost
-: "https://focusmeforked-production.up.railway.app";
+export const baseURL = "https://focusme.up.railway.app";
