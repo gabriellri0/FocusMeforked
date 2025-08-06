e9c3bf338d12d7f60e5a05ef968ad6ae09e3ae39
Gabriel Rio Tinto Cavalcanti <griotintocavalcanti@gmail.com>
2025-08-06 18:01:10 -0300

    reverter commit a

--- frontend/src/componentes/api.js
+++ frontend/src/componentes/api.js
@@ -1,2 +1,5 @@
-baseURL = "https://focusme.up.railway.app/"
+const isLocalhost = window. location. hostname == "localhost";
 
+export const baseURL = isLocalhost
+? "http://localhost:8000/" // Localhost
+: "https://focusmeforked-production.up.railway.app";
